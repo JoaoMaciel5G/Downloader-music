2bb7e3f1b212ffe59cdbd69d9171c0d6fc87ed78
JoaoMaciel5G <juauvitu765@gmail.com>
2024-02-12 16:35:21 -0300

    change getFormats

--- src/app/api/getFormats/route.ts
+++ src/app/api/getFormats/route.ts
@@ -26,6 +26,6 @@ export async function GET(request: NextRequest) {
 
         return NextResponse.json(info_video, {status: 200})
     }catch(error){
-        return NextResponse.json({error: error.message}, {status: 500})
+        return NextResponse.json({error: error}, {status: 500})
     }
 }
